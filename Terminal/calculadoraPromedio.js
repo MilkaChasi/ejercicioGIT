b8030c01a5390670aead8c5fe4503572e332477e
Milka Chasi <mchasijimenez@gmail.com>
2024-01-04 22:53:25 -0500

    Añadir código de la calculadora de promedio

--- Terminal/calculadoraPromedio.js
+++ Terminal/calculadoraPromedio.js
@@ -1,26 +1,37 @@
 let nombreAlumno = prompt("Ingrese el nombre del alumno")
 let Materia = prompt("Ingrese la materia")
 
-// Solicitar las tres calificaciones 
-let C1 = parseFloat(prompt("Ingresar la primera calificacion"))
-let C2 = parseFloat(prompt("Ingresar la segunda calificacion"))
-let C3 = parseFloat(prompt("Ingresar la tercera calificacion"))
+// Solicitar las tres calificaciones y sus verificaciones
+while (true) {
+    let C1 = parseFloat(prompt("Ingresar la primera calificación"))
+    if (isNaN(C1) && C1 >= 0 && C1 <= 10) {
+        break
+    } else { console.log("Revisar la primera calificación del/la alumno/a:" + nombreAlumno) }
+}
 
-//Verificacion de calificaciones, rango: 0-10
+while (true) {
+    let C2 = parseFloat(prompt("Ingresar la segunda calificación"))
+    if (isNaN(C2) && C2 >= 0 && C2 <= 10) {
+        break
+    } else { console.log("Revisar la segunda calificación del/la alumno/a:" + nombreAlumno) }
+}
 
-if (isNaN(C1) || isNaN(C2) || isNaN(C3) || C1 < 0 || C1 > 10 || C2 < 0 || C2 > 10 || C3 < 0 || C3 > 10) {
-    console.log("Revisar las calificaciones del/la alumno/a:" + nombreAlumno)
+while (true) {
+    let C3 = parseFloat(prompt("Ingresar la tercera calificación"))
+    if (isNaN(C3) && C3 >= 0 && C3 <= 10) {
+        break
+    } else { console.log("Revisar la tercera calificación del/la alumno/a:" + nombreAlumno) }
+}
+
+
+// Calculo del promedio
+let promedio = (C1 + C2 + C3) / 3
+
+// Condicion del promedio
+
+if (promedio >= 7) {
+    console.log(nombreAlumno + ", ¡Felicidades! Has aprobado con un promedio de:" + promedio)
 } else {
-
-    // Calculo del promedio
-    let promedio = (C1 + C2 + C3) / 3
-
-    // Condicion del promedio
-
-    if (promedio >= 7) {
-        console.log(nombreAlumno + ", ¡Felicidades! Has aprobado con un promedio de:" + promedio)
-    } else {
-        console.log(nombreAlumno + ", gracias por tu esfuerzo. Nos vemos pronto en clases. El promedio obtenido es:" + promedio)
-    }
+    console.log(nombreAlumno + ", gracias por tu esfuerzo. Nos vemos pronto en clases. El promedio obtenido es:" + promedio)
 }
 
