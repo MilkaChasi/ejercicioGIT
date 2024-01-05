1d5ed72e2c1c06bafaaecfe486704c1ea75dd08b
Milka Chasi <mchasijimenez@gmail.com>
2024-01-04 23:41:43 -0500

    Añadir código de la calculadora de promedio

--- Terminal/calculadoraPromedio.js
+++ Terminal/calculadoraPromedio.js
@@ -2,25 +2,36 @@ let nombreAlumno = prompt("Ingrese el nombre del alumno")
 let Materia = prompt("Ingrese la materia")
 
 // Solicitar las tres calificaciones y sus verificaciones
+let C1;
+let C2;
+let C3;
+
 while (true) {
-    let C1 = parseFloat(prompt("Ingresar la primera calificación"))
-    if (isNaN(C1) && C1 >= 0 && C1 <= 10) {
+    C1 = parseFloat(prompt("Ingresar la primera calificación"));
+    if (!isNaN(C1) && C1 >= 0 && C1 <= 10)  {
         break
-    } else { console.log("Revisar la primera calificación del/la alumno/a:" + nombreAlumno) }
+    } else {
+        alert("Verificación: Revisar la primera calificación del/la alumno/a:" + nombreAlumno)
+    };
 }
 
 while (true) {
-    let C2 = parseFloat(prompt("Ingresar la segunda calificación"))
-    if (isNaN(C2) && C2 >= 0 && C2 <= 10) {
+    C2 = parseFloat(prompt("Ingresar la segunda calificación"));
+    if (!isNaN(C2) && C2 >= 0 && C2 <= 10)  {
         break
-    } else { console.log("Revisar la segunda calificación del/la alumno/a:" + nombreAlumno) }
+    } else {
+        alert("Verificación: Revisar la segunda calificación del/la alumno/a:" + nombreAlumno)
+    };
 }
 
+
 while (true) {
-    let C3 = parseFloat(prompt("Ingresar la tercera calificación"))
-    if (isNaN(C3) && C3 >= 0 && C3 <= 10) {
+    C3 = parseFloat(prompt("Ingresar la tercera calificación"));
+    if (!isNaN(C3) && C3 >= 0 && C3 <= 10)  {
         break
-    } else { console.log("Revisar la tercera calificación del/la alumno/a:" + nombreAlumno) }
+    } else {
+        alert("Verificación: Revisar la tercera calificación del/la alumno/a:" + nombreAlumno)
+    };
 }
 
 
